a506b6d50a9ea761c0284dd0c5e764bf362d0003
Deepak Patel <pateldeepak7691@gmail.com>
2019-11-11 13:11:45 +0000

    commit(2) -HelloWorld.java

--- src/sample/java/project/HelloWorld.java
+++ src/sample/java/project/HelloWorld.java
@@ -1,7 +1,13 @@
 package sample.java.project;
 
-class HelloWorld{  
-    public static void main(String args[]){  
-     System.out.println("Hello World...");  
+class HelloWorld{
+    public static void main(String args[]){
+     System.out.println("Hello World...");
+     aMethod();
     }
+
+    public static void aMethod(){
+      System.out.println("You are awesome!");
+    }
+
 }
