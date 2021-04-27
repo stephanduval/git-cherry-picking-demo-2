8752043ec88493d0c729ceb425211ffdf366d2c8
Stephan DuVal <stephan.duval@gmail.com>
2021-04-26 19:34:39 -0600

    commit(2) -HelloWorld.java

--- src/sample/java/project/HelloWorld.java
+++ src/sample/java/project/HelloWorld.java
@@ -1,13 +1,13 @@
 package sample.java.project;
 
 class HelloWorld{
-    public static void main(String args[]){
-     System.out.println("Hello World...");
-     aMethod();
-    }
+	public static void main(String args[]){
+		System.out.println("Hello World..");
+		aMethod();
+	}
 
-    public static void aMethod(){
-      System.out.println("You are awesome!");
-    }
 
+	public static void aMethod(){
+		System.out.println("You are awesome!");
+	}
 }
